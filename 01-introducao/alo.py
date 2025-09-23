1f525644d54477820e23268aa778512952510443
Galileu Batista <gbatmobile@gmail.com>
2025-09-23 10:05:20 -0300

    Introducao

---
+++ 01-introducao/alo.py
@@ -0,0 +1,9 @@
+print ("allo")
+print ("Aqui sera exibido", "2*3")
+print ("Aqui sera exibido", 2*3)
+print ("6*6/6*6 = ", 6*6/6*6)
+print ("Eu tenho", 56, "anos, ", "Gio tem 21")
+
+ano_nascimento = 2008
+idade = 2025 - ano_nascimento
+print ("A idade é:", idade)
